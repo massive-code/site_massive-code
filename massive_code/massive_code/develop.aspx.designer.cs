4368aec0ce3cd0efaa0a54a7c53477074c3ca4f3
Sanctuar <it.security@list.ru>
2016-07-09 15:53:25 +0300

    develop page

--- massive_code/massive_code/develop.aspx.designer.cs
+++ massive_code/massive_code/develop.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace massive_code {
     
     
     public partial class develop {
-        
-        /// <summary>
-        /// Label2 элемент управления.
-        /// </summary>
-        /// <remarks>
-        /// Автоматически создаваемое поле.
-        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }
