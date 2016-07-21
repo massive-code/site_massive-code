37b490c04930f997eb8aa7468aad163355c752f1
Sanctuar <it.security@list.ru>
2016-07-21 15:23:31 +0300

    image slider ajax

--- massive_code/massive_code/union_client.aspx.designer.cs
+++ massive_code/massive_code/union_client.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace massive_code {
         protected global::System.Web.UI.WebControls.Button Button_Download_UnionClient;
         
         /// <summary>
+        /// ASPxImageSlider1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxImageSlider ASPxImageSlider1;
+        
+        /// <summary>
         /// Button_GIT_Click элемент управления.
         /// </summary>
         /// <remarks>
@@ -29,32 +38,5 @@ namespace massive_code {
         /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button_GIT_Click;
-        
-        /// <summary>
-        /// Image1 элемент управления.
-        /// </summary>
-        /// <remarks>
-        /// Автоматически создаваемое поле.
-        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
-        
-        /// <summary>
-        /// Image2 элемент управления.
-        /// </summary>
-        /// <remarks>
-        /// Автоматически создаваемое поле.
-        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image2;
-        
-        /// <summary>
-        /// Image3 элемент управления.
-        /// </summary>
-        /// <remarks>
-        /// Автоматически создаваемое поле.
-        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image3;
     }
 }
--- massive_code/massive_code/union_server.aspx.designer.cs
+++ massive_code/massive_code/union_server.aspx.designer.cs
@@ -31,30 +31,12 @@ namespace massive_code {
         protected global::System.Web.UI.WebControls.Button Button_GIT_Click;
         
         /// <summary>
-        /// Image1 элемент управления.
+        /// ASPxImageSlider1 элемент управления.
         /// </summary>
         /// <remarks>
         /// Автоматически создаваемое поле.
         /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
-        
-        /// <summary>
-        /// Image2 элемент управления.
-        /// </summary>
-        /// <remarks>
-        /// Автоматически создаваемое поле.
-        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image2;
-        
-        /// <summary>
-        /// Image3 элемент управления.
-        /// </summary>
-        /// <remarks>
-        /// Автоматически создаваемое поле.
-        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image3;
+        protected global::DevExpress.Web.ASPxImageSlider ASPxImageSlider1;
     }
 }
